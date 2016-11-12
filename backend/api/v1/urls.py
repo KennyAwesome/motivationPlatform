1f40e54db44831d859b750da5bd35cf6ec4a8bcd
Helge Bruegner <helge@bruegner.de>
2016-11-12 17:12:11 +0100

    changed some small things

--- backend/api/v1/urls.py
+++ backend/api/v1/urls.py
@@ -6,6 +6,6 @@ app_name = 'api-v1'
 
 urlpatterns = [
     url(r'^ping/', views.PingView.as_view()),
-    url(r'^webhook/(?P<application>[a-z0-9]*)/$', views.WebhookView.as_view())
-
+    url(r'^webhook/(?P<application>[a-z0-9]*)/$', views.WebhookView.as_view()),
+    url(r'^update/(?P<device_id>[a-z0-9]*)', views.UpdateView.as_view())
 ]
--- backend/api/v1/views.py
+++ backend/api/v1/views.py
@@ -1,4 +1,5 @@
 from django.http import HttpResponse, JsonResponse
+from django.http import HttpResponseNotFound
 from django.views import View
 
 
@@ -11,3 +12,22 @@ class WebhookView(View):
     def get(self, request, application):
         if application == 'wunderlist':
             return HttpResponse('works')
+        else:
+            return HttpResponseNotFound('The webhook for \'' + application + '\' is not activated')
+
+
+class UpdateView(View):
+    def get(self, request, device_id):
+        if device_id == 'nao':
+            return JsonResponse({
+                'tasks': [
+                    {
+                        'id': 12345,
+                        'due_date': '2013-08-30T08:36:13.273Z',
+                        'starred': True,
+                        'title': 'Clean the dishes'
+                    }
+                ]
+            })
+        else:
+            return HttpResponseNotFound('The device \'' + device_id + '\' could not be found')
