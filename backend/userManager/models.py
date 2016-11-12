548ca0b6aa866f9dc9fefe4a398b742cc9038f38
Kenny <info@avatify.com>
2016-11-12 07:13:00 +0100

    Registration works, Login has form error. Giving it up, for the sake of prototypical scheduler

--- backend/userManager/models.py
+++ backend/userManager/models.py
@@ -1,5 +1,14 @@
 from __future__ import unicode_literals
 
 from django.db import models
-
+from django.core.urlresolvers import reverse
 # Create your models here.
+
+# class Tokens(models.Model):
+#     user = models.User
+#
+#     def get_absolute_url(self):
+#         return(reverse('userManager:detail',kwargs={'pk':self.pk})) #keyword args
+#
+#     def __str__(self):
+#         return self.album_title + ' - ' + self.artist
--- backend/userManager/views.py
+++ backend/userManager/views.py
@@ -1,3 +1,107 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.core.urlresolvers import  reverse_lazy
+from django.contrib.auth import  authenticate, login
+# authenticate -> checks in db if exists, create regular session ID
+#from django.views import generic
+from django.views.generic import View
+from django.views.generic.edit import CreateView
+from .forms import RegisterForm
+from .forms import LoginForm
 
+#from django.context_processor import csrf
+#from .models import # will be tokens
 # Create your views here.
+#album = get_object_or_404(Album, pk=album_id)
+
+#class IndexView(generic.ListView):
+#    template_name = 'userManager/templates/index.html'
+#    def get_queryset(self):
+#        return HttpResponse("<h1>Basic User thingy works</h1>")
+class IndexView(View):
+    template_name = 'index.html'
+
+    def get(self, request):
+        #form = self.form_class(None)
+        return render(request, self.template_name, "")
+
+class LoginView(View):
+    template_name = 'login.html'
+    form_class = LoginForm  # the blueprint
+
+    # display blank form -> no account
+    def get(self, request):
+        form = self.form_class(None)
+        return render(request, self.template_name, {'form': form})
+
+    def post(self, request):
+        form = self.form_class(request.POST) # hier is already validation
+        context = {'loggedIn': False, 'message': ""}
+
+        if form.is_valid():
+            # normalize data, proper format
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(username=username, password=password)
+
+            return render(request, "index.html", context)
+            if user is not None:
+                if user.is_active:
+                    login(request, user) # now logged in
+                    context = {'loggedIn': True}
+                    return render(request, "index.html", context)
+                else:
+                    context = {'loggedIn': False, 'message': "User is not active"}
+                    return render(request, "index.html", context)
+            else:
+                    context = {'loggedIn': False, 'message': "None user"}
+                    return render(request, "index.html", context)
+        else:
+            context = {'loggedIn': False, 'message': "Invalid Form"}
+            return render(request, "index.html", context)
+                    #request.user.username
+                    # should redirect, not to show blank page
+
+class RegisterView(View):
+    form_class = RegisterForm # the blueprint
+    template_name = 'form_registration.html'
+    # class based views
+
+    # display blank form -> no account
+    def get(self, request):
+        form = self.form_class(None)
+        return render(request, self.template_name, {'form': form})
+
+    # process form data, adding user
+    def post(self, request):
+        form = self.form_class(request.POST) # hier is already validation
+
+        if form.is_valid():
+            user = form.save(commit=False) # creates a form from object, not in db yet
+
+            # normalize data, proper format
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+
+            # set password etc., they are not txt, they are hashes
+            user.set_password(password)
+            user.save()
+            #user.is_active = True  # should be done by email and activation ticket, but just for prototyping
+            #user.save()
+
+            #authenticate
+            user = authenticate(username=username, password=password)
+
+            context = {'loggedIn': False}
+            if user is not None:
+                if user.is_active:
+                    login(request, user) # now logged in
+                    context = {'loggedIn': True}
+                    return render(request, "index.html", context)
+                else:
+                    return render(request, "index.html", context)
+            else:
+                    return render(request, "index.html", context)
+                    #request.user.username
+                    # should redirect, not to show blank page
